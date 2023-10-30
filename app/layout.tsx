32247c8821c51f31a64cf88da12cc51c148c5e01
Dinesh Shaw <idineshshaw7@gmail.com>
2023-10-30 23:14:52 +0530

    customise radix ui theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,13 @@
-import '@radix-ui/themes/styles.css';
+import "@radix-ui/themes/styles.css";
+import "./theme-config.css";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Theme } from '@radix-ui/themes';
+import { Theme } from "@radix-ui/themes";
 
 import NavBar from "./NavBar";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
 export const metadata: Metadata = {
   title: "Issue Navigator",
@@ -20,10 +21,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <Theme>
+      <body className={inter.variable}>
+        <Theme appearance="light" accentColor="violet">
           <NavBar />
-          <main className='p-5'>{children}</main>
+          <main className="p-5">{children}</main>
         </Theme>
       </body>
     </html>
