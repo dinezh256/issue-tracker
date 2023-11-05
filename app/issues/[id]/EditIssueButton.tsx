4f53cb68932f337e262385d38551f611fcdca253
Dinesh Shaw <idineshshaw7@gmail.com>
2023-11-05 10:59:16 +0530

    added a delete button

--- app/issues/[id]/EditIssueButton.tsx
+++ app/issues/[id]/EditIssueButton.tsx
@@ -1,13 +1,17 @@
 import { Pencil2Icon } from "@radix-ui/react-icons";
-import { Button } from "@radix-ui/themes";
+import { Button, Flex, Text } from "@radix-ui/themes";
 import Link from "next/link";
 import React from "react";
 
-const EditIssueButton = ({ issueId }: { issueId: number}) => {
+const EditIssueButton = ({ issueId }: { issueId: number }) => {
   return (
-    <Button>
-      <Pencil2Icon />
-      <Link href={`/issues/${issueId}/edit`}>Edit Issue</Link>
+    <Button className="cursor-pointer">
+      <Link href={`/issues/${issueId}/edit`}>
+        <Flex gap="2" align="center">
+          <Pencil2Icon />
+          <Text>Edit Issue</Text>
+        </Flex>
+      </Link>
     </Button>
   );
 };
