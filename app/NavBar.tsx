5030b17cf1d204b876ee5c5a7d6456bad6b7461c
Dinesh Shaw <idineshshaw7@gmail.com>
2023-11-08 12:32:00 +0530

    prisma adapter

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -4,9 +4,12 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { AiFillBug } from "react-icons/ai";
 import classNames from "classnames";
+import { useSession } from "next-auth/react";
+import { Box, Container, Flex } from "@radix-ui/themes";
 
 const NavBar = () => {
   const currentPath = usePathname();
+  const { status, data: session } = useSession();
 
   const links = [
     {
@@ -20,26 +23,40 @@ const NavBar = () => {
   ];
 
   return (
-    <nav className="flex space-x-6 px-5 h-14 items-center border-b mb-5">
-      <Link href="/">
-        <AiFillBug />
-      </Link>
-      <ul className="flex space-x-6">
-        {links.map(({ href, label }) => (
-          <li key={href}>
-            <Link
-              className={classNames({
-                "text-zinc-900": href === currentPath,
-                "text-zinc-500": href !== currentPath,
-                "hover:text-zinc-800 transition-colors": true,
-              })}
-              href={href}
-            >
-              {label}
+    <nav className="px-5 h-14 border-b mb-5 py-3">
+      <Container>
+        <Flex justify="between">
+          <Flex align="center" gap="3">
+            <Link href="/">
+              <AiFillBug />
             </Link>
-          </li>
-        ))}
-      </ul>
+            <ul className="flex space-x-6">
+              {links.map(({ href, label }) => (
+                <li key={href}>
+                  <Link
+                    className={classNames({
+                      "text-zinc-900": href === currentPath,
+                      "text-zinc-500": href !== currentPath,
+                      "hover:text-zinc-800 transition-colors": true,
+                    })}
+                    href={href}
+                  >
+                    {label}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </Flex>
+          <Box>
+            {status === "authenticated" && (
+              <Link href="/api/auth/signout">Log Out</Link>
+            )}
+            {status === "unauthenticated" && (
+              <Link href="/api/auth/signin">Login</Link>
+            )}
+          </Box>
+        </Flex>
+      </Container>
     </nav>
   );
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { Inter } from "next/font/google";
 import { Container, Theme } from "@radix-ui/themes";
 
 import NavBar from "./NavBar";
+import AuthProvider from "./auth/provider";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
@@ -22,12 +23,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.variable}>
-        <Theme appearance="light" accentColor="violet">
-          <NavBar />
-          <main className="p-5">
-            <Container>{children}</Container>
-          </main>
-        </Theme>
+        <AuthProvider>
+          <Theme appearance="light" accentColor="violet">
+            <NavBar />
+            <main className="p-5">
+              <Container>{children}</Container>
+            </main>
+          </Theme>
+        </AuthProvider>
       </body>
     </html>
   );
